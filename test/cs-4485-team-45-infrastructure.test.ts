542b669036dec081d27070fafd9c76b5071114c8
knk190001 <keith.karanu01@gmail.com>
2024-04-10 15:15:53 -0500

    Initial commit

---
+++ test/cs-4485-team-45-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cs4485Team45Infrastructure from '../lib/cs-4485-team-45-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cs-4485-team-45-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cs4485Team45Infrastructure.Cs4485Team45InfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
